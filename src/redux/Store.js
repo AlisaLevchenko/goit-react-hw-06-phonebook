0008f40f2001278c1ce56dcdf22e56fa063a5122
AlisaLevchenko <good.luck.odria@gmail.com>
2022-08-18 20:30:35 +0300

    Create React HW#6

---
+++ src/redux/Store.js
@@ -0,0 +1,40 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contacts/ContactsReducer';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  version: 1,
+  storage,
+  whitelist: ['items'],
+};
+
+const contactsPersistedReducer = persistReducer(
+  contactsPersistConfig,
+  contactsReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsPersistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export const persistor = persistStore(store);
